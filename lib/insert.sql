deef9b5868efd95a0bf5727b1a11c92aca4ace6f
Bailey Cage <bailey.cage@gmail.com>
2022-09-21 12:39:24 -0400

    commit tree 0c7aa4359bfca4c5fdf60961a728f51161e17edd

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 22, "M", "Brown", "Happy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 18, "M", "Spots", "Crazy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 22, "F", "Brown", "Happy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20, "M", "Brown", "Serious", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 12, "F", "Black", "Happy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 42, "M", "Green", "Grumpy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 33, "F", "Brown", "Happy", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 22, "M", "Brown", "Happy", TRUE);
